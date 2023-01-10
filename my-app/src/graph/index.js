907b6a6be99bbc2b2bbdd6d502101845a4fff8e1
J1min <aoiharu41@gmail.com>
2023-01-10 12:22:45 +0900

    Feat: add transition

--- my-app/src/graph/index.js
+++ my-app/src/graph/index.js
@@ -5,13 +5,12 @@ export default function Index() {
   const graphRef = useRef(null);
 
   useEffect(() => {
-    const value = [100, 10, 20, 50, 120, 60];
+    const value = [100, 10, 20, 50, 10, 60, 100];
     const canvas = d3.select(graphRef.current);
     const config = {
       width: value.length * 40,
-      height: Math.max(...value) + 10,
-      x: 0,
-      y: 0,
+      height: Math.max(...value) + 50,
+      interval: 40,
       bar: {
         width: 30,
       },
@@ -23,14 +22,28 @@ export default function Index() {
       .attr("height", config.height)
       .attr("style", "background: #e9e9e9");
 
-    value.forEach((item, idx) =>
+    value.forEach((item, idx) => {
       svg
         .append("rect")
-        .attr("x", idx * 40)
-        .attr("y", config.height - item)
+        .attr("x", idx * config.interval)
+        .attr("y", config.height)
         .attr("width", config.bar.width)
         .attr("height", item)
-    );
+        .attr("fill", "#000000")
+        .transition()
+        .duration(1500)
+        .attr("fill", item === Math.max(...value) ? "#00aaff" : "#000000")
+        .attr("y", config.height - item);
+
+      svg
+        .append("text")
+        .attr("x", idx * config.interval + 5)
+        .attr("y", config.height - item - 10)
+        .text(item)
+        .attr("font-family", "sans-serif")
+        .attr("font-size", "12px");
+
+    });
   }, []);
 
   return <div ref={graphRef} />;
--- my-app/src/test/index.js
+++ my-app/src/test/index.js
@@ -1,4 +1,2 @@
 const array = [1, 2, 3, 4, 5];
-console.log(array.map((data) => data + "등"));
-console.log(array.filter((data) => !(data % 2)));
-console.log(array.filter((data) => !(data % 2)).map((data) => data + "등"));
+console.log(array.indexOf(Math.max(...array)));
